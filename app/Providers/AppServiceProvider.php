9e12218e6b9f4fe07536005c8da6313b93bea7b8
Jeremy Martin <jeremymartin06@gmail.com>
2020-11-03 17:08:09 +0100

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Blade;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('route', function ($expression) {
+            return "<?php echo route($expression); ?>";
+        });
+    }
+}
