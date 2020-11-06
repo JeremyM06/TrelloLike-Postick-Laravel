e93e40b562b8d9a46f3490391e94a7d4ed016264
ab elec <ab.electricien@yahoo.com>
2020-11-06 11:17:50 +0100

    pull

--- resources/views/tables.blade.php
+++ resources/views/tables.blade.php
@@ -13,9 +13,45 @@
 
             @foreach ($cards as $card)
                 @if ($card->col_id == $col->id)
-                    <div class="text-center border">
-                        {{ $card->title }}
-                    </div>
+                {{ $card->title }}
+                    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+    <div class="text-center">
+        {{ $card->title }}
+    </div>
+  </button>
+
+   {{-- Modal --}}
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Laissez un commentaire</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        @foreach ($coms as $com)
+            @if ($com->card_id == $card->id)
+                {{ $com->comment }}
+            @endif
+        @endforeach
+
+        <form action="@route('com.store')" method="POST">
+                <div class="modal-body">
+                    @csrf
+                    <input type="text" name="title">
+                    <input type="hidden" name="card_id" value=" {{ $card->id }} ">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                    <input type="submit" class="btn btn-primary" value="Enregistrer">
+                </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
                 @endif
             @endforeach
 
@@ -40,10 +76,5 @@
         <input type="hidden" name="tableId" value="{{ $table }}">
         <input type="submit">
     </form>
-<p> {{ $value->id }} </p>
-
-Macarte
-
-id de mon tableau: {{ $table }}
 
 @endsection
